44c414e6336cb9fe3a82b28232974847cc8743bc
sineth-Fdo <sinethinosh100@gmail.com>
2025-01-30 17:40:57 +0530

    chore: Jwt stratergy updates

--- src/stratergy/jwt.strategy.ts
+++ src/stratergy/jwt.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtService } from '@nestjs/jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { AuthService } from 'src/auth/auth.service';
 import { AuthJwtPayload } from 'src/auth/types/auth-jwtPayload';
 
@@ -9,21 +9,21 @@ import { AuthJwtPayload } from 'src/auth/types/auth-jwtPayload';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     private jwtService: JwtService,
-    private authService: AuthService, // To verify the user
+    private authService: AuthService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'your_jwt_secret', // Use a secure key or environment variable
+      secretOrKey: process.env.JWT_SECRET, 
     });
   }
 
   async validate(payload: AuthJwtPayload) {
     // Validate the user by the JWT payload
-    const user = await this.authService.findById(payload.sub); // Implement this method
+    const user = await this.authService.findById(payload.sub); 
     if (!user) {
       throw new UnauthorizedException('User not found');
     }
-    return user; // This will attach the user to the request object
+    return user; 
   }
 }
